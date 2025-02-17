2fed3ed780959ab02564449d92b177e30bdbfcbd
HamzaKhan9 <hamzalodhi03163045212@gmail.com>
2025-02-17 20:33:09 +0500

    fix: try catch added to upsert function

--- src/services/profile/index.ts
+++ src/services/profile/index.ts
@@ -10,39 +10,44 @@ const upsert = async (record: {
   name: ProfileInfo["name"];
   newProfilePic?: File | null;
 }) => {
-  const { id, newProfilePic, profile_photo, name } = record;
+  try {
+    const { id, newProfilePic, profile_photo, name } = record;
 
-  const storageBucket = supabase.storage.from("default");
-  if (profile_photo && newProfilePic) {
-    const prevTimeStamp = profile_photo.match(/-(\d+)\.png$/)![1];
-    await storageBucket.remove([`${id}/profileAvatar-${prevTimeStamp}.png`]);
-  }
+    const storageBucket = supabase.storage.from("default");
+    if (profile_photo && newProfilePic) {
+      const prevTimeStamp = profile_photo.match(/-(\d+)\.png$/)![1];
+      await storageBucket.remove([`${id}/profileAvatar-${prevTimeStamp}.png`]);
+    }
 
-  const updatedData: Partial<ProfileInfo> = {
-    name: name,
-  };
+    const updatedData: Partial<ProfileInfo> = {
+      name: name,
+    };
 
-  if (newProfilePic) {
-    const imagePath = `${id}/profileAvatar-${Date.now()}.png`;
-    await storageBucket.upload(imagePath, newProfilePic);
-    const { data: pathData } = storageBucket.getPublicUrl(imagePath);
-    updatedData["profile_photo"] = pathData.publicUrl;
-  }
-  console.log("updatedData: ", updatedData);
+    if (newProfilePic) {
+      const imagePath = `${id}/profileAvatar-${Date.now()}.png`;
+      await storageBucket.upload(imagePath, newProfilePic);
+      const { data: pathData } = storageBucket.getPublicUrl(imagePath);
+      updatedData["profile_photo"] = pathData.publicUrl;
+    }
+    console.log("updatedData: ", updatedData);
 
-  const { error } = await supabase
-    .from("profiles")
-    .update(updatedData)
-    .eq("id", id);
+    const { error } = await supabase
+      .from("profiles")
+      .update(updatedData)
+      .eq("id", id);
 
-  if (error) {
+    if (error) {
+      globalErrorHandler(error);
+      return null;
+    }
+    return {
+      ...record,
+      profile_photo: updatedData.profile_photo || profile_photo,
+    };
+  } catch (error) {
     globalErrorHandler(error);
     return null;
   }
-  return {
-    ...record,
-    profile_photo: updatedData.profile_photo || profile_photo,
-  };
 };
 
 const create = async (record: {
